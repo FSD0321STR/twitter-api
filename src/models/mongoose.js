2fcae2d4a18fb502db1f05b64ce00c1489906ed4
Agustindlf <dlfagustin@hotmail.com>
2021-06-25 21:19:17 +0200

    Login y register backend

--- src/models/mongoose.js
+++ src/models/mongoose.js
@@ -14,10 +14,10 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const userSchema = new mongoose.Schema({
     email: String,
     password: String,
-    boards: [
+    tweets: [
         {
             type: mongoose.Schema.Types.ObjectId,
-            ref: 'Board'
+            ref: 'Tweet'
         }
     ],
     createdAt: { type: Date, default: Date.now },
@@ -35,13 +35,41 @@ userSchema.pre('save', async function (next) {
 const User = mongoose.model('User', userSchema);
 
 const Tweet = mongoose.model('Tweet', {
-    title: String,
+    body: String,
+    images: [{
+        type: mongoose.Types.ObjectId,
+        ref: "TweetImage" , 
+    }],
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+});
+
+const TweetImage = mongoose.model('TweetImage', {
+    img: {
+        data: Buffer,
+        contentType: String, 
+    },
+    tweet: {
+        type: mongoose.Types.ObjectId,
+        ref: "Tweet", 
+    },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now },
 });
 
 
-
+const TweetCollection = mongoose.model('TweetCollection', {
+    user: {
+        type: mongoose.Types.ObjectId,
+        ref: "User", 
+    },
+    tweets: [{
+        type: mongoose.Types.ObjectId,
+        ref: "Tweet", 
+    }],
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+});
 
 
 
@@ -61,5 +89,7 @@ validateTweet = (document, method) => {
 module.exports = {
     Tweet,
     validateTweet,
-    User
+    User,
+    TweetImage,
+    TweetCollection,
 }
